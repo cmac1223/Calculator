9e93c0d354bc84909f4ed7d539de7b1ee1361fab
Chris McClain <chrismac539@gmail.com>
2022-07-07 08:10:51 -0400

    added basic conditional chain for desire outcome

--- calculator.js
+++ calculator.js
@@ -35,6 +35,10 @@ divide = (a, b) => {
 }
 
 // operate function takes an operator and 2 numbers then calls one of the above function on the numbers.
-operate = ( operator, a, b) => {
-  
+operate = (operator, a, b) => {
+  return operator === add ? add(a, b)
+    : operator === subtract ? subtract(a, b)
+      : operator === multiply ? multiply(a, b)
+        : operator === divide ? divide(a, b)
+        : 'You must select an two numbers and operator'        
 }
