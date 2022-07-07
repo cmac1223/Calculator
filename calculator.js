6d358fe5fa52464bf7c10246f12587e69460a968
Chris McClain <chrismac539@gmail.com>
2022-07-06 21:07:31 -0400

    created add function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+add = (a, b) => {
+  if (Number.isInteger(a) === true && Number.isInteger(b) === true) {
+    return a + b;
+  } else {
+    return 'You must select an number'
+  }
+}
