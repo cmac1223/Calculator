661c02012af910475629d06fa4edac2e98b00279
Chris McClain <chrismac539@gmail.com>
2022-07-27 08:19:48 -0400

    edit operate function

--- calculator.js
+++ calculator.js
@@ -35,7 +35,7 @@ divide = (a, b) => {
 }
 
 // operate function takes an operator and 2 numbers then calls one of the above function on the numbers.
-operate = (operator, a, b) => {
+operate = (a, operator, b) => {
   return operator === add ? add(a, b)
     : operator === subtract ? subtract(a, b)
       : operator === multiply ? multiply(a, b)
