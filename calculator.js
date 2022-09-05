de3b5f8db8de9fbb03fce8be6fbf00336686d075
Chris McClain <chrismac539@gmail.com>
2022-09-05 18:02:11 -0400

    setup addEventListener with buttons clicked

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
 const calculator = document.querySelector('.calculator');
-const key = calculator.querySelector('.calculator__keys');
+const keys = calculator.querySelector('.calculator__keys');
 const display = document.querySelector('.calculator__display');
+
+// function fires when button is click
+keys.addEventListener('click', e =>{
+  if (e.target.matches('button')){
+    const key = e.target;
+    console.log(key);
+  }
+})
