bf138738500397336a7200e7abf44890fc3580db
Chris McClain <chrismac539@gmail.com>
2022-09-05 17:35:54 -0400

    display value targets display div

--- calculator.js
+++ calculator.js
@@ -1,2 +1,3 @@
 const calculator = document.querySelector('.calculator');
 const key = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
